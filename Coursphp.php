14406d7e75fe5970609dc16f464e23f148505cc8
sackoyakouba <yakoubasacko2023@gmail.com>
2025-11-25 11:38:54 +0000

    premier commit

---
+++ Coursphp.php
@@ -0,0 +1,10 @@
+<?php
+$villes = ["Fria","Conakry","Labe","Zerekore","Kankan"];
+// parcourt du tableau
+foreach($villes as $ville){
+    echo $ville,"<br>";
+}
+
+// Affichage du nombre total des villes
+   echo "le nombre total de villes est: ".count($villes);
+   ?>
